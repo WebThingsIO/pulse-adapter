333ed5cbb06eb657016dcdbf1d43c8cb8189fa19
Michael Stegeman <michael@stegeman.me>
2020-03-03 13:57:43 -0900

    Update workflows.

--- package.sh
+++ package.sh
@@ -1,5 +1,11 @@
 #!/bin/bash -e
 
-rm -f SHA256SUMS
-sha256sum -- manifest.json package.json *.js LICENSE README.md > SHA256SUMS
-npm pack
+rm -rf node_modules
+
+shasum --algorithm 256 manifest.json package.json *.js LICENSE README.md > SHA256SUMS
+
+TARFILE=`npm pack`
+
+shasum --algorithm 256 ${TARFILE} > ${TARFILE}.sha256sum
+
+rm -rf SHA256SUMS package
